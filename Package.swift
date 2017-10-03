f4b7f7551b7592dcf7f38188b02119abed16f55b
Jonathan Guthrie <jono@perfect.org>
2017-10-03 15:13:57 -0400

    update for swift 4 libs

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-04 16:25:29 +0000
+// Date: 2017-10-03 19:09:29 +0000
 import PackageDescription
 let package = Package(
-    name: "CouchDBStORM-Demo",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/SwiftORM/CouchDB-StORM.git", majorVersion: 1),
-    ]
+	name: "CouchDBStORM-Demo",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/SwiftORM/CouchDB-StORM.git", majorVersion: 3),
+	]
 )
